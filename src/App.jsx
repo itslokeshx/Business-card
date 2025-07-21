372d72cd15c64f95612dcf5db074e8a366ba1d1a
itslokeshx <lokeshvijayraina@gmail.com>
2025-07-22 00:51:35 +0530

    organized manner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import About from "../components/About.jsx";
-import Detail from "../components/Detail.jsx";
-import Footer from "../components/Footer.jsx";
-import Interest from "../components/Interest.jsx";
+import About from "./components/About.jsx";
+import Detail from "./components/Detail.jsx";
+import Footer from "./components/Footer.jsx";
+import Interest from "./components/Interest.jsx";
 
 export default function App() {
   return (
