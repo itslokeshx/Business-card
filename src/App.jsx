d2f412393fbd9467685da8d21b55ee1e740e1a6c
itslokeshx <lokeshvijayraina@gmail.com>
2025-07-22 00:49:52 +0530

    organized manner

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from "../components/About.jsx";
+import Detail from "../components/Detail.jsx";
+import Footer from "../components/Footer.jsx";
+import Interest from "../components/Interest.jsx";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Detail />
+      <About />
+      <Interest />
+      <Footer />
+    </div>
+  );
+}
